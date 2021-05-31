c68d88a48a2e9455d9265d0ec1c523f19c0717c6
NguyenThanhViet <nguyenthanhviet31031998@gmail.com>
2021-05-31 21:06:01 +0700

    Build user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Tony', password: '12345678')
+User.create(username: 'Tom', password: '12345678')
+User.create(username: 'Apple', password: '12345678')
+User.create(username: 'Rose', password: '12345678')
+User.create(username: 'Rachel', password: '12345678')
