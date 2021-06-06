20e4b6fb3cf851325b2a08d791447d76fd1b007e
NguyenThanhViet <nguyenthanhviet31031998@gmail.com>
2021-06-06 18:55:34 +0700

    Build realtime chat feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,12 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+};
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
 
@@ -27,4 +33,6 @@ $(document).on('turbolinks:load', function() {
       .transition('fade')
     ;
   });
+
+  scroll_bottom();
 });
---
+++ app/assets/javascripts/channels/chatroom.coffee
@@ -0,0 +1,11 @@
+App.chatroom = App.cable.subscriptions.create "ChatroomChannel",
+  connected: ->
+    # Called when the subscription is ready for use on the server
+
+  disconnected: ->
+    # Called when the subscription has been terminated by the server
+
+  received: (data) ->
+    $("#message-container").append(data.modify_message);
+    $("#message_body").val('');
+    scroll_bottom();
